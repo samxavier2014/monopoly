d5a9b72f418ff0dd0585aa497f59ac837af5c8d3
samuelxavier2014 <samxavier995@gmail.com>
2026-07-01 17:14:39 -0700

    new file

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+import pygame
+pygame.init()
 
-print("hello World!")
+WIDTH = 600
+HEIGHT = 200
+pygame.draw.rect(600)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Monopoly Junior")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    BACKGROUND = (220, 240, 220)
+    BOARD = (245, 245, 220)
+    BLACK = (0, 0, 0)
+    font = pygame.font.SysFont(None, 48)
+    screen.fill((220,240,220))
+    pygame.display.flip()
+
+pygame.quit()
